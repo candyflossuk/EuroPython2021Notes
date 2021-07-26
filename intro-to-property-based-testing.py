a63d4d42006e4407e8f063d1f3dd35924be90259
candyflossuk <ross.a.humphrey@googlemail.com>
2021-07-26 10:07:28 +0100

    Added an example of a wrapper for executing functions
    Added wrapper for function execution using annotations

--- intro-to-property-based-testing.py
+++ intro-to-property-based-testing.py
@@ -8,3 +8,25 @@ with open('file.txt') as fobj_in, \
         open('out.txt', 'w') as fobj_out:
     fobj_out.write(fobj_in.read())
 
+
+class C:
+    # Example of decorator using static method
+    @staticmethod
+    def func(self):
+        return 42
+
+    # equivalent to the below - where the staticmethod annotation is not used.
+    # func = staticmethod(func)
+
+# Create a wrapper that prints 'Wrapped' before executing the function
+def hello(func):
+    def wrapper(*args, **kwargs):
+        print('Wrapped')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@hello
+def add(a, b):
+    return a + b
+
